072ebcba275c727406575630e82ccdc9edd7fd31
Varshaj2907 <varshaj2907@gmail.com>
2016-05-01 23:31:07 +0530

    Update hashing.cpp

--- hashing.cpp
+++ hashing.cpp
@@ -1,3 +1,11 @@
+//============================================================================
+// Name        : hash.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -12,9 +20,7 @@ int hashalgo(string str,int prime)
 	int i=0;
 	while(c=str[i++])
 	{
-		cout<<c<<endl;
 		hash=((hash<<5)+hash)+c;
-		/* hash * 33 + c */
 	}
 	return hash%prime;
 }
@@ -91,7 +97,7 @@ public:
 class Hashfile {
 	string filename;
 	int buckets;
-  int b_size;
+    int b_size;
 	int rsize;
 public:
 	Hashfile(string fname, int no_of_buckets,int bucket_size, int recsize)
@@ -110,41 +116,45 @@ public:
 		f.close();
 	}
 	int addrec(string s,string key)
-	{
-		int bucket_nos=hashalgo(key,buckets);
-	  char buff[rsize];
-	  fstream f(filename.c_str(),ios::in|ios::out);
-		f.seekg(bucket_nos*b_size*rsize);
-		for(int i=0;i<b_size;i++)
 		{
-	     int offset=f.tellg();
-			 f.read(buff,rsize);
-	     if(buff[0]=='$')
-	       {
-	           f.seekp(offset);
-	           f<<s;
-	           f.close();
-	           return 0;
-	       }
-	   }
-	  f.close();
-	  return -1;
-	 }
+			int bucket_nos=hashalgo(key,buckets);
+	        char buff[rsize];
+	        fstream f(filename.c_str(),ios::in|ios::out);
+			f.seekg(bucket_nos*b_size*rsize);
+			for(int i=0;i<b_size;i++)
+			{
+	            int offset=f.tellg();
+			    f.read(buff,rsize);
+	            if(buff[0]=='$')
+	            {
+	            	f.seekp(offset);
+	            	f<<s;
+	                f.close();
+	                return 0;
+	             }
+	         }
+			f.close();
+	        return -1;
+	      }
 	string search(string key,string value)
 	{
 		char buffer[rsize+1];
 		fstream f(filename.c_str(),ios::in|ios::out);
 		buffer[rsize] = '\0';
-    int bucket_no = hashalgo(key,buckets);
-		f.seekg(bucket_no * b_size * rsize);
-		for(int i = 0; i < buckets; i++)
+        int bucket_no = hashalgo(key,buckets);
+		f.seekg(0,ios::beg);
+		for(int i = 0; i < (buckets * b_size ); i++)
 		{
+			//cout<<"pos in file search :"<<f.tellp()<<endl;
 		    f.read(buffer,rsize);
             if ( buffer[0] != '$' )
             {
+            	//cout<<"buffer : "<<buffer<<endl<<endl;
             	Student s = Student::unpack(buffer);
             	if (s.match(key,value))
+            	{
             		return buffer;
+            	}
             }
 		}
 	return "";
@@ -153,6 +163,8 @@ public:
 int main()
 {
 	int n;
+	char ch;
+	string key;
 	cout<<"Enter the Number of Buckets"<<endl;
 	cin>>n;
 	Hashfile f("data.txt",n,5,100);
@@ -166,6 +178,18 @@ int main()
 	f.addrec(s3.pack(100),s3.getkey());
 	f.addrec(s4.pack(100),s4.getkey());
 	f.addrec(s5.pack(100),s5.getkey());
-	cout << f.search("name","d") << endl;
+	while(1)
+	{
+	cout<<"Do you want to search?(y/n)";
+	cin>>ch;
+	if(ch=='y'|| ch=='Y')
+		{
+		cout<<"Enter key to be searched : ";
+		cin>>key;
+		cout << f.search("name",key) << endl;
+		}
+	else
+		break;
+	}
 	return 0;
 }
